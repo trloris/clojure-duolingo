ea18d56999a69c246bb17c031a3be2c276d8dab1
Trevyn Loris <trevyn.loris@gmail.com>
2015-03-05 21:34:29 -0600

    Add get-settings func.

--- src/duolingo/core.clj
+++ src/duolingo/core.clj
@@ -6,3 +6,8 @@
   "Retrieve user info from Duolingo in form of JSON"
   [user]
   (json/read-str (:body (client/get (str "http://duolingo.com/users/" user)))))
+
+(defn get-settings
+  "Retrieve user's info from dictionary"
+  [user-dict]
+  (select-keys user-dict ["notify_comment" "deactivated" "is_follower_by" "is_following"]))
