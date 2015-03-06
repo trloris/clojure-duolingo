a0add608ce34af5e43cc8923b10ea5f1b7fa4438
Trevyn Loris <trevyn.loris@gmail.com>
2015-03-05 21:23:18 -0600

    Remove default function.

--- src/duolingo/core.clj
+++ src/duolingo/core.clj
@@ -2,12 +2,7 @@
   (:require [clj-http.client :as client]
             [clojure.data.json :as json]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
-
 (defn get-user
-  "google thing"
+  "Retrieve user info from Duolingo in form of JSON"
   [user]
   (json/read-str (:body (client/get (str "http://duolingo.com/users/" user)))))
