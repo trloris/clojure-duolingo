2e8d59c5b544bdf05f53ff4df0a80cbe7901f6da
Trevyn Loris <trevyn.loris@gmail.com>
2015-03-05 22:10:24 -0600

    Add get-language-progress

--- src/duolingo/core.clj
+++ src/duolingo/core.clj
@@ -10,4 +10,27 @@
 (defn get-settings
   "Retrieve user's info from dictionary"
   [user-dict]
-  (select-keys user-dict ["notify_comment" "deactivated" "is_follower_by" "is_following"]))
+  (select-keys user-dict ["notify_comment" "deactivated" "is_follower_by"
+                          "is_following"]))
+
+(defn get-languages
+  "Retrieve the list of languages the user is learning"
+  [user-dict]
+  (filter #(get % "learning") (user-dict "languages")))
+
+(defn get-user-info
+  "Retrieve user's info"
+  [user-dict]
+  (select-keys user-dict ["username" "bio" "id" "num_following" "cohort"
+                          "num_followers" "learning_language_string" "created"
+                          "contribution_points" "gplus_id" "twitter_id" "admin"
+                          "invites_left" "location" "fullname" "avatar"
+                          "ui_language"]))
+
+(defn get-language-progress
+  "Retrieve progress of user's selected language"
+  [user-dict language]
+  (select-keys ((user-dict "language_data") language)
+  	["streak" "language_string" "level_progress" "num_skills_learned"
+     "level_percent" "level_points" "points_rank" "next_level" "level_left"
+     "language" "points"]))
